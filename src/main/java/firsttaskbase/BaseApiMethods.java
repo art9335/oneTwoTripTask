42b41e17de6482c63382d68a8a9259fe372ffa20
druart <art9335@yandex.ru>
2020-08-05 20:00:37 +0300

    Добавил тесты на api

--- src/main/java/firsttaskbase/BaseApiMethods.java
+++ src/main/java/firsttaskbase/BaseApiMethods.java
@@ -5,11 +5,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.util.Set;
 
 public class BaseMethods {
     public static void main(String[] args) throws UnsupportedEncodingException {
@@ -32,6 +34,47 @@ public class BaseMethods {
         }
         return fullStringCode;
     }
+
+    public static String formatUrlForRequest(String urlBody, String urlPart, String requestData, int l) {
+        String resultUrl = urlBody.concat(urlPart);
+        resultUrl = String.format(resultUrl, requestData, l);
+        return resultUrl;
+    }
+    public static JSONArray  getKeySet(JSONObject arg) {
+        /*
+        для получения значений ключей json
+        */
+        Set<String> arrayList = arg.keySet();
+        JSONArray newArray = new JSONArray();
+        for (String s : arrayList) {
+            System.out.println(s.toString());
+            newArray.put(s);
+        }
+        return newArray;
+    }
+
+    public static JSONArray getJsonArray(JSONObject arg, String keyValue) {
+        /*
+        для получения массива
+        */
+        JSONArray nestedArray = arg.getJSONArray(keyValue);
+        return nestedArray;
+    }
+
+    public static JSONObject getNestedJsonByIndex(JSONArray arg, int indexJson) {
+        /*
+        для получения вложенных  json-тел в массив
+
+        */
+        JSONObject nestedJson = arg.getJSONObject(indexJson);
+        return nestedJson;
+    }
+
+    public static JSONObject getJsonByKey(JSONObject arg, String keyValue) {
+        JSONObject nestedJson = arg.getJSONObject(keyValue);
+        return nestedJson;
+    }
+
 
     public static String toHexWithPersentFormat(String arg) throws UnsupportedEncodingException {
         String f = String.format("%04x", new BigInteger(1, arg.getBytes()));
